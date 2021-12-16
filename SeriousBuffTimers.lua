4bb841369443315d755638eb48595855a2a8e7ba
Macumba <85767653+Macumbatbc@users.noreply.github.com>
2021-12-16 15:15:13 +0100

    Update to match with TBCC
    avoid a bad hooksecurefunc

--- SeriousBuffTimers.lua
+++ SeriousBuffTimers.lua
@@ -7,24 +7,22 @@
 
 -- Hook Blizzy's function. What? You want to hook it too? TOUGH TITTIES!
 
-function SecondsToTimeAbbrev(time)
-    local m
+local function SecondsToTimeAbbrev(button, time)
+    local duration = _G[button:GetName().."Duration"]
+    local m, text
     if( time <= 0 ) then
-        return "";
+        text = "";
     elseif time < 60 then
-        return string.format("%d", time);
+        text = duration:SetFormattedText("%d", time);
     elseif( time < 3600 ) then
         m = math.floor(time / 60)
         local s = math.fmod(time, 60)
-        return string.format("%d:%02d", m, s);
+        text = duration:SetFormattedText("%d:%02d", m, s);
     else
         local hr = math.floor(time / 3600)
         m = math.floor( math.fmod(time, 3600) / 60 )
-        return string.format("%d.%02dhr", hr, m);
+        text = duration:SetFormattedText("%d.%02dhr", hr, m);
     end
+    return text
 end
-
-hooksecurefunc("AuraButton_Update", function(self, index)
-		local button = _G[self..index]
-		if (button and not button.styled) then applySkin(button) end
-end)
+hooksecurefunc("AuraButton_UpdateDuration", SecondsToTimeAbbrev) 
